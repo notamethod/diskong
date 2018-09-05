01e86203f7efa2fb2d3c652e0d80384f50c532e1
croger42 <croger42@gmail.com>
2018-09-05 20:54:56 +0200

    change close windows behavior/test github push 3

--- kong-app/src/main/java/diskong/app/detail/DetailForm.java
+++ kong-app/src/main/java/diskong/app/detail/DetailForm.java
@@ -70,7 +70,7 @@ public class DetailForm extends JDialog implements EventListener {
     AlbumVo albumOri;
 
     public DetailForm(AlbumVo albumOri) throws IOException {
-        //Test txo
+        //Test txo test 3
         this.albumOri = albumOri;
         setContentPane(contentPane);
         setModal(true);
